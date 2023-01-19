836f6c8a3ecbc2dea1fdc8cc2722fc026e6565c3
Saarthakk14 <122539546+Saarthakk14@users.noreply.github.com>
2023-01-19 08:58:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(311, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onGesture(Gesture.TiltRight, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+	
+})
