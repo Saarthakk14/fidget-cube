4b09ee15ef2a4bcf56b639ab8697e10afa887c81
Saarthakk14 <122539546+Saarthakk14@users.noreply.github.com>
2023-01-20 08:57:38 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,44 @@
 input.onButtonPressed(Button.A, function () {
-    music.playTone(311, music.beat(BeatFraction.Whole))
-    music.playTone(349, music.beat(BeatFraction.Whole))
+	
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    music.playMelody("F G E C5 G C G - ", 120)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showString("Fidget Cube")
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    basic.showLeds(`
+        . # # # .
+        # # # . .
+        # # . . .
+        # # # . .
+        . # # # .
+        `)
 })
 input.onGesture(Gesture.TiltRight, function () {
-	
+    basic.showIcon(IconNames.Duck)
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
-	
+    basic.showLeds(`
+        . # # # .
+        # # # . .
+        # # . . #
+        # # # . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # # # .
+        # # . . .
+        # # # # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
 })
